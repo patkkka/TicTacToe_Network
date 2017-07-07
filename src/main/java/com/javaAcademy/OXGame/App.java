7c57c83df408e64101e2c640f961f4b30863e2a9
patrycja <patrycja_zaremba@epam.com>
2017-07-07 16:29:31 +0200

    MsgResolver to be finished

--- src/main/java/com/javaAcademy/OXGame/App.java
+++ src/main/java/com/javaAcademy/OXGame/App.java
@@ -1,7 +1,7 @@
 package com.javaAcademy.OXGame;
 
+import java.io.PrintStream;
 import java.util.Locale;
-import java.util.ResourceBundle;
 import java.util.Scanner;
 
 import com.javaAcademy.OXGame.helper.MessageResolver;
@@ -12,23 +12,22 @@ import com.javaAcademy.OXGame.model.Symbol;
 
 
 public class App {
-	
-	Scanner s = new Scanner(System.in);
-	
-    public static void main( String[] args ) {
-    	App app = new App();
-    	
-    	System.out.println("Please choose the language/Wybierz język: 1 - English, 2 - Polish.");
-    	final int lang = Integer.parseInt(app.s.nextLine());
-    	app.createMessageResolver(lang);
-    	GameSettings settings = app.getGameSettings();
-    	GameStatistics statistics = app.getGameStatistics();
-    	
-    	Game.startGame(settings, statistics);
-    }
-    
-    private void createMessageResolver(int langNumber) {
-		switch(langNumber) {
+
+	public static void main(String[] args) {
+		App app = new App();
+		UserIO io = new UserIO(System.out,System.in);
+
+		final int lang = Integer.parseInt(io.userMessageWithInput(
+				"Please choose the language/Wybierz język: 1 - English, 2 - Polish"));
+		app.createMessageResolver(lang);
+		GameSettings settings = app.getGameSettings(io);
+		GameStatistics statistics = app.getGameStatistics(io);
+
+		Game.startGame(settings, statistics);
+	}
+
+	private void createMessageResolver(int langNumber) {
+		switch (langNumber) {
 			case 1:
 				MessageResolver.createMessageResolver(new Locale("en", "EN"));
 				break;
@@ -40,37 +39,31 @@ public class App {
 		}
 	}
 
-	private GameStatistics getGameStatistics() {
-    	System.out.println("Please enter O player nickname: ");
-    	String oNickname = s.nextLine();
-    	
-    	System.out.println("Please enter X player nickname: ");
-    	String xNickname = s.nextLine();
+	private GameStatistics getGameStatistics(UserIO io) {
+		MessageResolver msgResolver = MessageResolver.msgResolverInstance();
+		String oNickname = io.userMessageWithInput(msgResolver.getMsgByKey("string.playerONickname"));
+		String xNickname = io.userMessageWithInput(msgResolver.getMsgByKey("string.playerXNickname"));
 
-    	Player xPlayer = new Player(xNickname, Symbol.X);
-    	Player oPlayer = new Player(oNickname, Symbol.O);
-    	return new GameStatistics(xPlayer, oPlayer);
-    }
-    
-    private GameSettings getGameSettings() {
-    	MessageResolver msgResolver = MessageResolver.msgResolverInstance();
-    	//TODO choosing languange
-    	System.out.println(msgResolver.getMsgByKey("empty.chosenLanguage"));
-    	
-    	System.out.println(msgResolver.getMsgByKey("int.xDimension"));
-    	final int xDim = Integer.parseInt(s.nextLine());
-    	
-    	System.out.println("Choose board Y dimension: ");
-    	final int yDim = Integer.parseInt(s.nextLine());
-    	//TODO board dimension validation
-    	
-    	System.out.println("Choose how many characters to win: ");
-    	//TODO char series validation
-    	final int charSeriesDim = Integer.parseInt(s.nextLine());
-    	
-    	System.out.println("Choose who start: press 1 if O, press 2 if X:");
-    	//TODO char validation
-    	final int whoStarts = Integer.parseInt(s.nextLine());
+		Player xPlayer = new Player(xNickname, Symbol.X);
+		Player oPlayer = new Player(oNickname, Symbol.O);
+		return new GameStatistics(xPlayer, oPlayer);
+	}
+
+	private GameSettings getGameSettings(UserIO io) {
+		MessageResolver msgResolver = MessageResolver.msgResolverInstance();
+		//TODO choosing languange
+		io.showUserMessage(msgResolver.getMsgByKey("empty.chosenLanguage"));
+
+		final int xDim = Integer.parseInt(io.userMessageWithInput(msgResolver.getMsgByKey("int.xDimension")));
+		final int yDim = Integer.parseInt(io.userMessageWithInput(msgResolver.getMsgByKey("int.yDimension")));
+		//TODO board dimension validation
+
+		final int charSeriesDim = Integer.parseInt(io.userMessageWithInput(msgResolver.getMsgByKey("int.winningCondition")));
+		//TODO char series validation
+
+		System.out.println("Choose who start: press 1 if O, press 2 if X:");
+		//TODO char validation
+		final int whoStarts = Integer.parseInt(s.nextLine());
 		return new GameSettings(whoStarts, charSeriesDim, xDim, yDim, "en", 3);
-    }
+	}
 }
